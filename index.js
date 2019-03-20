8d6ae6fd8f9adc43c7de9eb35fdb498d042a13ba
alekx3 <alekx3@outlook.com>
2019-03-20 01:04:06 +0100

    added promises on exec

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = function (sails) {
 
     var setup = require('./lib/setup.js');
+    var setup = require('./lib/exec.js');
     var getRobot = require('./lib/neato/neatoGetRobot.js');
     var startCleaning = require('./lib/neato/neatoStartCleaning.js');
     var stopCleaning = require('./lib/neato/neatoStopCleaning.js');
