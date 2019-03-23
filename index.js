004a45d419fcab2c619b33a5ba3e59145ea1f5db
alekx3 <alekx3@outlook.com>
2019-03-23 18:53:31 +0100

    fixing angular1 FE issue

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ module.exports = function (sails) {
 
     var getRobot = require('./lib/getRobot');
     var test = require('./lib/test');
-    var configure = require('./lib/configuration');
+    var setup = require('./lib/setup');
 
     return {
         test,
+        setup,
         getRobot,
         configure
     };
