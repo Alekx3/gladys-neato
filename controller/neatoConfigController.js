b99606e5e44df330ac48af46b0df416447729fc9
alekx3 <alekx3@outlook.com>
2019-03-23 18:22:40 +0100

    fixing angular1 FE issue

--- controller/neatoConfigController.js
+++ controller/neatoConfigController.js
@@ -3,7 +3,12 @@ saveConfig = require('../lib/configuration.js');
 module.exports = {
 
   saveConfig: function(req, res, next) {
-     console.log(req)
+     console.log(req);
+     result = {
+         "success" : "yes"
+     }
+     res.json(result);
+
     /*saveConfig(req.body)
       .then((result) => res.json(result))
       .catch(next);*/
