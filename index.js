18d5012b1a93d82d3014db3be7f1e992d7904bc9
alekx3 <alekx3@outlook.com>
2019-03-23 02:28:49 +0100

    correction on handler

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 module.exports = function (sails) {
 
-    var setup = require('./lib/setup.js');
+    //var setup = require('./lib/setup.js');
     //var exec = require('./lib/exec.js');
     var getRobot = require('./lib/getRobot');
-
+    var test = require('./lib/test');
 
 
     return {
-        setup,
+        //set\up,
         //exec,
+        test,
         getRobot,
     };
 };
