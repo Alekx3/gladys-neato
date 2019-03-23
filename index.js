d575debef3e000136d2c40d7e2bb13b18a5b8cf5
alekx3 <alekx3@outlook.com>
2019-03-23 16:03:44 +0100

    added functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (sails) {
 
     var getRobot = require('./lib/getRobot');
     var test = require('./lib/test');
-    var configure = require('./lib/setup');
+    var configure = require('./lib/configuration');
 
     return {
         test,
