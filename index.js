e8f8053d49d37a43be0ca6eaf6f9a8fecc6e4580
alekx3 <alekx3@outlook.com>
2019-03-23 23:34:36 +0100

    implemented start robot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function (sails) {
 
     var getRobot = require('./lib/getRobot');
     var test = require('./lib/test');
+    var startRobot = require('./lib/startRobot');
     var setup = require('./lib/setup');
     var exec = require('./lib/exec');
     
@@ -9,6 +10,7 @@ module.exports = function (sails) {
         test,
         setup,
         getRobot,
+        startRobot,
         exec
     };
 };
