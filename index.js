7f74df0acad28cef11fab7016488237de21eced1
alekx3 <alekx3@outlook.com>
2019-03-23 21:32:54 +0100

    fix on exec()

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ module.exports = function (sails) {
     var getRobot = require('./lib/getRobot');
     var test = require('./lib/test');
     var setup = require('./lib/setup');
-
+    var exec = require('./lib/exec');
+    
     return {
         test,
         setup,
-        getRobot
+        getRobot,
+        exec
     };
 };
